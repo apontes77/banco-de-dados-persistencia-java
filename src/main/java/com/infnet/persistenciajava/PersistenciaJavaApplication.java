985c9131f9c0f024cf8a411996eb5f3967a89ad7
Alexandre Queiroz <alexandrepontesgyn@gmail.com>
2022-10-18 20:14:22 -0300

    first commit

---
+++ src/main/java/com/infnet/persistenciajava/PersistenciaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.persistenciajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistenciaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infnet/persistenciajava/PersistenciaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.persistenciajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
